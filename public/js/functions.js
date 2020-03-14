7a853dcdf2a36a5ba2cdbc4554fea768f6b94c9b
Leonidas Yopan <leonidasyopan@gmail.com>
2020-03-14 13:01:18 -0300

    application working with item displayed late.

--- public/js/functions.js
+++ public/js/functions.js
@@ -13,8 +13,51 @@ function getItems() {
                 console.log(err.message);
             }
 
-            document.getElementById("itemDetails").innerHTML = JSON.stringify(data);
+            console.log(JSON.stringify(data));
+
+            let beforeStorageDate = JSON.stringify(data);
+
+            console.log(beforeStorageDate);
+            
+            localStorage.setItem("currentListOfItems", beforeStorageDate);
+
+            var afterStorageDate = JSON.parse(localStorage.getItem("currentListOfItems")); 
+
+            console.log(afterStorageDate);
+
+            return afterStorageDate;
         }
     }
     ajax.send();
 }
+
+function buildHTMLDisplay() {
+    console.log("Entered buildHTMLDisplay function");
+    
+    var dataFromDB = JSON.parse(localStorage.getItem("currentListOfItems"));  
+
+    console.log(dataFromDB);
+
+    /* Create a div to hold all the info of the Selected team */
+    var output = '';    
+    for (var i=0; i < dataFromDB.length; i++){
+        output += '<div id="item-box">';
+        output += "<h2>" + dataFromDB[i].product_name + "</h2>";    
+        output += '<figure class="image-item"><img src="' + dataFromDB[i].product_image + '" alt="' + dataFromDB[i].product_name + ' Thumb"></figure>';
+        output += '<div id="item-data">';
+        output += "<p><strong>Price:</strong> " + dataFromDB[i].product_price + "</p>";
+        output += "<p><strong>Description:</strong> " + dataFromDB[i].product_description + "</p>";
+        output += "<p><strong>Items in Stock:</strong> " + dataFromDB[i].product_stock + "</p>";                     
+        output += "</div>";
+        output += "</div>";
+    }
+
+    document.getElementById("itemDetails").innerHTML = output;
+
+    
+}
+
+let getItemButton = document.querySelector("#getItemButton");
+
+getItemButton.addEventListener("click", getItems);
+getItemButton.addEventListener("click", buildHTMLDisplay);
