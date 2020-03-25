ad75ff45e526cd3127bd847c837226076b792023
Leonidas Yopan <leonidasyopan@gmail.com>
2020-03-24 22:52:37 -0300

    defined structure for login, not working yet.

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ const express = require('express');
 const path = require('path');
 require('dotenv').config();
 
+// IMPORTING SESSION STUFF
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
+
 const PORT = process.env.PORT || 5000;
 var app = express();
 
+// IMPORTING ALL CONTROLLERS
 const getItemController = require("./controllers/getItemController.js")
 const productController = require("./controllers/productController.js")
 const userController = require("./controllers/userController.js")
@@ -12,12 +17,32 @@ const userController = require("./controllers/userController.js")
 app.set("port", PORT);
 
 app.use(express.static(path.join(__dirname, 'public')))
+
+// Body parser middleware to use post values
 app.use(express.json()); // support JSON encoded bodies
 app.use(express.urlencoded({extended: true})); // support URL encoded bodies
 
+// USE SESSION
+app.use(session({
+    name: 'delicious-cookie-id',
+    secret: 'some important secret',
+    saveUninitialized: true,
+    resave: true,
+    store: new FileStore()
+}))
+
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
-// app.get('/', (req, res) => res.render('pages/index'))
+
+app.use(userController.logRequest);
+
+// Setup our routes
+app.post('/login', userController.handleLogin);
+app.post('/logout', userController.handleLogout);
+
+// This method has a middleware function "verifyLogin" that will be called first
+app.get('/getServerTime', userController.verifyLogin, userController.getServerTime);
+
 app.get('/', (request, response) => {
     // response.render("pages/getItems");    
     response.render("pages/index");    
@@ -28,6 +53,10 @@ app.get('/manage-products', (request, response) => {
 
 app.get('/register-user', (request, response) => {
     response.render("pages/register-user");    
+})
+
+app.get('/login-user', (request, response) => {
+    response.render("pages/login-user");    
 })
 
 app.post("/register", userController.handleRegister);
